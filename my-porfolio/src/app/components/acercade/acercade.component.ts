687bcb12a3bbd204fb025a57633eb9867b310d2a
Maria Florencia <121450160+primeriza@users.noreply.github.com>
2023-05-15 09:43:31 -0300

    interpolacion
    Hice interpolación en el componente "acerca de".

--- my-porfolio/src/app/components/acercade/acercade.component.ts
+++ my-porfolio/src/app/components/acercade/acercade.component.ts
@@ -7,13 +7,13 @@ import { PorfolioService } from 'src/app/servicios/porfolio.service';
   styleUrls: ['./acercade.component.css']
 })
 export class AcercadeComponent implements OnInit {
- 
+ miPorfolio:any;
   constructor(private datosPorfolio:PorfolioService){}
 
   ngOnInit(): void {
     this.datosPorfolio.obtenerDatos().subscribe(data =>{
       console.log(data);
-
+      this.miPorfolio=data;
     });
   
   }
--- my-porfolio/src/app/components/educacion/educacion.component.ts
+++ my-porfolio/src/app/components/educacion/educacion.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { PorfolioService } from 'src/app/servicios/porfolio.service';
 
 @Component({
   selector: 'app-educacion',
   templateUrl: './educacion.component.html',
   styleUrls: ['./educacion.component.css']
 })
-export class EducacionComponent {
+export class EducacionComponent  {
+education:any;
+constructor(private datosPorfolio:PorfolioService){}
+
+
+ngOnInit(): void {
+  this.datosPorfolio.obtenerDatos().subscribe(data =>{
+    console.log(data);
+    this.education=data;
+  });
 
 }
+
+}
+
+
+
+
